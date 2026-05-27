2982aa93d4fd6d2ff1dc5ecc71f4c75a129b1a89
Poly Shestak <polyshestak@ucsb.edu>
2026-05-27 15:52:34 -0700

    added return at the end of the function

--- climate_function.R
+++ climate_function.R
@@ -11,20 +11,27 @@
 #' 
 #' precip (optimal = 20-30in/yr)
 climate_score = function(precip, temp) {
+  
   if (precip < 5 | precip > 40) {
     precip_score = 0
-  } else if (precip >=20 & precip <=30) {
+  } else if (precip >= 20 & precip <= 30) {
     precip_score = 2.5
   } else {
     precip_score = 1.25
   }
   
   # temp (optimal 60-80)
-  if(temp < 50 | temp > 90) {
+  if (temp < 50 | temp > 90) {
     temp_score = 0
-  } else if (temp >=60 & temp <=80) {
+  } else if (temp >= 60 & temp <= 80) {
     temp_score = 2.5 
   } else {
     temp_score = 1.25
   }
+  
+  climate_score = precip_score + temp_score
+  
+  return(climate_score)
 }
+
+
