766d7a8d9aa2bd814e079862c78ae396150c8efe
georgiagoldsmith <georgiagoldsmith@ucsb.edu>
2026-05-26 20:39:10 -0700

    test data set, fertilitity score, climate score

--- tomato_growth_function.R
+++ tomato_growth_function.R
@@ -8,4 +8,4 @@
 #' @param fertilizer_score fert. suitability score (0-3in/yr)
 #' @param climate_score climate suitability score (0 to 5 in/yr)
 #' @param soil_health_score soil health score (0 to 4in/yr)
-#' @return tomato_growth - predicted tomato crop growht (0-12 in/yr)
+#' @return tomato_growth - predicted tomato crop growth (0-12 in/yr)
