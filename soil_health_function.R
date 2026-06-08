c3ccc27a34013eeee9edd902a250461ee691ad2b
georgiagoldsmith <georgiagoldsmith@ucsb.edu>
2026-06-08 14:20:38 -0700

    documentation

---
+++ soil_health_function.R
@@ -0,0 +1,10 @@
+#| label: functions
+#| include: true
+#| echo: false
+#| message: false
+
+#' Module 3 - Soil Health Score
+#' @param pH soil pH (0 to 14)
+#' @param soil_om soil om content (%)
+#' @return soil_health_score - soil health suitability score (0 to 4 in/yr)
+#' 
